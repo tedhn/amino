76243da54f58b7f3ec8d959f39a645acbfce5d2d
Ted <heinhtetnaing186@gmail.com>
2019-12-27 20:57:54 +0630

    Updates

---
+++ precache-manifest.888be9fd47196b015a2fc4d180e48d09.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39050c8d99d514f4d8be763d20b69d5e",
+    "url": "/amino/index.html"
+  },
+  {
+    "revision": "c031e014928de369aed9",
+    "url": "/amino/static/css/main.94e12b7b.chunk.css"
+  },
+  {
+    "revision": "d95d5a1dcfa8dcc9d01d",
+    "url": "/amino/static/js/2.0c75a124.chunk.js"
+  },
+  {
+    "revision": "c031e014928de369aed9",
+    "url": "/amino/static/js/main.3c3c817d.chunk.js"
+  },
+  {
+    "revision": "032b1904d7c4d58a83e8",
+    "url": "/amino/static/js/runtime-main.0de0e198.js"
+  },
+  {
+    "revision": "770104608e7436b6c5b4d2efb27a14a6",
+    "url": "/amino/static/media/Codes.77010460.svg"
+  },
+  {
+    "revision": "82a816a3d49de990a5735169a7d2c311",
+    "url": "/amino/static/media/CompanyName.82a816a3.svg"
+  },
+  {
+    "revision": "c4e2ac1607e92f53edc3d50b0a42a576",
+    "url": "/amino/static/media/Faq.c4e2ac16.svg"
+  },
+  {
+    "revision": "18bfed1de1a87e7190f83a5142e9291c",
+    "url": "/amino/static/media/Freelance.18bfed1d.svg"
+  },
+  {
+    "revision": "6856eca539e944570ca3f8948b29c8cf",
+    "url": "/amino/static/media/Loading.6856eca5.svg"
+  },
+  {
+    "revision": "29f318a4e010647bc072a1057ce10f5c",
+    "url": "/amino/static/media/Main.29f318a4.svg"
+  },
+  {
+    "revision": "23628be309e2e45f35aa8c039c23017a",
+    "url": "/amino/static/media/Main2.23628be3.svg"
+  },
+  {
+    "revision": "0f99390f164e28cced763e20fca96487",
+    "url": "/amino/static/media/Main3.0f99390f.svg"
+  },
+  {
+    "revision": "fe64798e467b2100b981bec29069a519",
+    "url": "/amino/static/media/Team.fe64798e.svg"
+  }
+]);
